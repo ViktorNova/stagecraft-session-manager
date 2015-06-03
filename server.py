230320a57d3cd5faf933f205e0be6fe3af9b03fd
ViktorNova <viktor@pinknoiseboys.com>
2015-06-03 11:48:01 -0700

    Now launches it's own instance of nsmd

--- server.py
+++ server.py
@@ -2,16 +2,34 @@
 #  Using http://www.codestance.com/tutorials-archive/python-tornado-web-server-with-websockets-part-i-441
 #  as a starting point
 # http://www.tornadoweb.cn/en/documentation#module-index
+from rdflib.plugins.parsers.pyRdfa.rdfs.process import subPropertyOf
 import tornado.ioloop
 import tornado.web
-import time
+import os
+import subprocess
+
 import liblo
 import jack
 
 from tornado.options import define, options, parse_command_line
 
-
 define("port", default=7777, help="run on the given port", type=int)
+
+nsmd_port = "7000"
+session_root = "/home/stagecraft/NSM Dev Sessions"
+nsmd = subprocess.Popen(['nsmd',
+                         '--osc-port', nsmd_port,
+                         '--session-root', session_root],
+                        stdout=subprocess.PIPE)
+pid = nsmd.pid
+
+print("Started nsmd daemon with pid ", repr(pid))
+
+# Pipe all output from the nsmd subprocess and show it on the python console
+nsmd_output = iter(nsmd.stdout.readline, b"")
+for line in nsmd_output:
+    print(line)  # yield line
+
 
 class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
